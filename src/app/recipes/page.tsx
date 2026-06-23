b63a156a1bd1cb5c6737c3d7113a8ff19c8fdb36
Noah Erickson <ericksonnoah5@gmail.com>
2026-06-23 00:39:59 -0500

    web test

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,9 +1,21 @@
 "use client";
+import router from "next/router";
 
 export default function Page() {
+  function recipies() {
+    router.push(`recipes/page.tsx`);
+  }
+  function dashboard() {
+    router.push(`dashboard/page.tsx`);
+  }
+  function fitness() {
+    router.push(`/page.tsx`);
+  }
   return (
     <>
-      <h1>hi</h1>
+      <button onClick={fitness}>hi</button>
+      <button onClick={recipies}>Recipes</button>
+      <button onClick={dashboard}>Dashboard</button>
     </>
   );
 }
