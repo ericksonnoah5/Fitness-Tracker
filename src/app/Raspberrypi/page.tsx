a6a066e1a7b53b6d6c53f54f8c15cbfc1adb2668
Noah Erickson <ericksonnoah5@gmail.com>
2026-06-22 22:43:30 -0500

    track Raspberrypi as regular directory

---
+++ src/app/Raspberrypi/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+
+export default function page() {
+  return <h1>Dashboard</h1>;
+}
