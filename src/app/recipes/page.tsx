cc5e420dc84f85d7ec1282e6ed09618d07c28c5b
Noah Erickson <ericksonnoah5@gmail.com>
2026-06-23 23:57:04 -0500

    page

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -5,11 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function page() {
   return (
     <>
-      <Button variant="outline">Outline</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="destructive">Delete</Button>
-      <Button variant="link">Link style</Button>
+      <Button variant="outline">Button</Button>
     </>
   );
 }
